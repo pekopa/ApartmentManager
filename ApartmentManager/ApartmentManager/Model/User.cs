e165694c03aedc3e4e1bda92b6a64fa93f5c9eec
Donatas Adamonis <dona0169@edu.easj.dk>
2017-04-28 17:55:36 +0200

    User Model

---
+++ ApartmentManager/ApartmentManager/Model/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApartmentManager.Model
+{
+   public class User
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int Phone { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Email { get; set; }
+        public int ApartmentNr { get; set; }
+
+        public User() { }
+
+        public User(string Name, string LastName, int Phone, DateTime BirthDate, string Email, int ApartmentNr)
+        {
+            this.Name = Name;
+            this.LastName = LastName;
+            this.Phone = Phone;
+            this.BirthDate = BirthDate;
+            this.Email = Email;
+            this.ApartmentNr = ApartmentNr;
+
+        }
+
+        //public override string ToString()
+        //{
+        //    return string.Format("Hotelno {0} Name {1} Address {2}", Hotel_No, Name, Address);
+        //}
+
+    }
+}
