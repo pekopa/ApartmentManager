e7b3fd74a4824f60c72d83c53b190f6fb3820861
Donatas Adamonis <dona0169@edu.easj.dk>
2017-05-17 19:12:38 +0200

    defectviewPage

--- ApartmentManager/ApartmentManager/View/ApartmentDefectViewPagexaml.xaml.cs
+++ ApartmentManager/ApartmentManager/View/ApartmentDefectViewPagexaml.xaml.cs
@@ -20,16 +20,11 @@ namespace ApartmentManager.View
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class ApartmentDefectPage : Page
+    public sealed partial class ApartmentDefectViewPagexaml : Page
     {
-        public ApartmentDefectPage()
+        public ApartmentDefectViewPagexaml()
         {
             this.InitializeComponent();
-        }
-
-        private void NavigateNewDefect(object sender, RoutedEventArgs e)
-        {
-            Frame.Navigate(typeof(ApartmentNewDefect));
         }
     }
 }
