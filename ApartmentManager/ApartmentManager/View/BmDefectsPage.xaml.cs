e40237d0eeb72691ac90658265219cd66a34a623
Marcin Zelent <zelent.marcin@protonmail.com>
2017-05-27 15:03:13 +0200

    Added empty pages for future use.

---
+++ ApartmentManager/ApartmentManager/View/BmDefectsPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace ApartmentManager.View
+{
+    /// <summary>
+    /// Page for managing defects.
+    /// </summary>
+    public sealed partial class BmDefectsPage : Page
+    {
+        public BmDefectsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
